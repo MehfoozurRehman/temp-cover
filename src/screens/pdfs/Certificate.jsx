435b29ac2eab14831767075c6cbe0e62471cc233
Asim Rasheed <asimrasheed442@gmail.com>
2024-09-03 16:05:34 +0500

    Update Certificate.jsx

--- src/screens/pdfs/Certificate.jsx
+++ src/screens/pdfs/Certificate.jsx
@@ -46,7 +46,6 @@ export default function Certificate() {
               padding: "15px 25px 0px",
               display: "flex",
               flexDirection: "column",
-              fontFamily: "Arial",
             }}
           >
             <View
@@ -62,7 +61,7 @@ export default function Certificate() {
               <Text
                 style={{
                   fontSize: "16px",
-
+                  fontFamily: "Arial",
                   fontWeight: "bold",
                 }}
               >
@@ -78,16 +77,200 @@ export default function Certificate() {
                 padding: "0px 8px",
               }}
             ></View>
-            <View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: "30px",
+                paddingLeft: "70px",
+              }}
+            >
               <Text
                 style={{
-                  fontSize: "14px",
-
-                  fontWeight: "bold",
+                  fontSize: "16px",
+                  fontFamily: "Arial",
                 }}
               >
                 CERTIFICATE OF MOTOR INSURANCE
               </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                marginTop: "7px",
+                paddingLeft: "70px",
+                fontFamily: "Arial",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "11px",
+                }}
+              >
+                Certificate Number:TCV-MOT-1739398
+              </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingTop: "20px",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "10px",
+                  fontFamily: "Arial",
+                }}
+              >
+                (1)
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: "10px",
+                  paddingLeft: "10px",
+                  fontFamily: "Arial",
+                }}
+              >
+                Vehicle Registration Number:
+              </Text>
+              <Text
+                style={{
+                  fontSize: "10px",
+                  fontWeight: "medium",
+                  paddingLeft: "5px",
+                }}
+              >
+                444XDX
+              </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingTop: "20px",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "10px",
+                  fontFamily: "Arial",
+                }}
+              >
+                (2)
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: "10px",
+                  paddingLeft: "10px",
+                  fontFamily: "Arial",
+                }}
+              >
+                Insured:
+              </Text>
+              <Text
+                style={{
+                  fontSize: "10px",
+                  fontWeight: "medium",
+                  paddingLeft: "5px",
+                }}
+              >
+                Mohammed Hasham Iqbal
+              </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "row",
+                  paddingTop: "20px",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    fontFamily: "Arial",
+                  }}
+                >
+                  (3)
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    paddingLeft: "10px",
+                    fontFamily: "Arial",
+                  }}
+                >
+                  Effective Time/Date:
+                </Text>
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    fontWeight: "medium",
+                    paddingLeft: "5px",
+                  }}
+                >
+                  10 August 2024 22:03
+                </Text>
+              </View>
+              <View
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  alignItems: "center",
+                  flexDirection: "row",
+                  paddingTop: "20px",
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    fontFamily: "Arial",
+                  }}
+                >
+                  (4)
+                </Text>
+
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    paddingLeft: "10px",
+                    fontFamily: "Arial",
+                  }}
+                >
+                  Expiry Time/Date:
+                </Text>
+                <Text
+                  style={{
+                    fontSize: "10px",
+                    fontWeight: "medium",
+                    paddingLeft: "5px",
+                  }}
+                >
+                  17 August 2024 22:03
+                </Text>
+              </View>
             </View>
           </View>
         </Page>
