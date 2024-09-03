2344062782654c19d97e16308d45047457cbf353
Arslan Goursi <arsljiger431@gmail.com>
2024-09-03 13:25:06 +0500

    initials

---
+++ src/screens/Index.jsx
@@ -0,0 +1,10 @@
+import { Link } from "remixed";
+
+export default function Index() {
+  return (
+    <div>
+      Hello this is home page for form
+      <Link to="/about">About</Link>
+    </div>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,72 @@
+import { access, mkdir } from "fs/promises";
+
+import HotExport from "vite-plugin-hot-export";
+import { VitePWA } from "vite-plugin-pwa";
+import { ViteWebfontDownload } from "vite-plugin-webfont-dl";
+import { chunkSplitPlugin } from "vite-plugin-chunk-split";
+import { config } from "./remix.config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteCompression from "vite-plugin-compression";
+import viteImagemin from "vite-plugin-imagemin";
+
+const alias = {
+  remixed: "/src/remixed",
+  "@router": "/router",
+  "@layouts": "/src/layouts",
+  "@": "/src",
+  ...config.alias,
+};
+
+async function createFoldersIfNeeded() {
+  try {
+    const assetsFolder = "./src/assets";
+    const componentsFolder = "./src/components";
+
+    await Promise.all([
+      access(assetsFolder).catch(() => mkdir(assetsFolder)),
+      access(componentsFolder).catch(() => mkdir(componentsFolder)),
+    ]);
+  } catch (error) {
+    console.error("Error creating folders:", error);
+  }
+}
+
+createFoldersIfNeeded();
+
+export default defineConfig({
+  resolve: { alias },
+  plugins: [
+    HotExport(),
+    config.compression && [
+      chunkSplitPlugin(),
+      viteCompression({
+        algorithm: "brotliCompress",
+        threshold: 100,
+      }),
+    ],
+    config.fontOptimization && ViteWebfontDownload(),
+    config.progressiveWebApp && VitePWA({ registerType: "autoUpdate" }),
+    config.imagesOptimization &&
+      viteImagemin(
+        config.imagesOptimizationOptions || {
+          gifsicle: { optimizationLevel: 7, interlaced: false },
+          optipng: { optimizationLevel: 7 },
+          mozjpeg: { quality: 30 },
+          webp: { quality: 70 },
+          svgo: {
+            multipass: true,
+            plugins: [
+              { name: "removeViewBox" },
+              { name: "minifyStyles" },
+              { name: "removeMetadata" },
+              { name: "removeUselessStrokeAndFill" },
+              { name: "reusePaths" },
+              { name: "removeEmptyAttrs", active: true },
+            ],
+          },
+        }
+      ),
+    react(),
+  ].filter(Boolean),
+});
