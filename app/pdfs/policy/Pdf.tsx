d1ccaa4b557d673716afedba15580852d302a3a8
Arslan Iftikhar <a44222666@gmail.com>
2024-09-04 20:54:08 +0500

    Update Pdf.tsx

--- app/pdfs/policy/Pdf.tsx
+++ app/pdfs/policy/Pdf.tsx
@@ -47,7 +47,7 @@ export default function Pdf({ data }: { data: any }) {
               padding: "15px 25px 0px",
               display: "flex",
               flexDirection: "column",
-              fontSize: "7px",
+              fontSize: "8px",
             }}
           >
             <View
@@ -614,6 +614,61 @@ export default function Pdf({ data }: { data: any }) {
                     </Text>
                   </View>
                 </View> */}
+              </View>
+            </View>
+
+            {/* horizontal line */}
+            <View style={{
+              marginTop: "10px",
+              height: "4px",
+              width: "100%",
+              backgroundColor: "black"
+            }}></View>
+
+
+            {/* 3rd table */}
+
+
+            <View style={{
+              border: "1.2px solid black",
+              padding: "5px",
+              marginTop: "15px"
+            }}>
+              <Text style={{ textDecoration: "underline", fontSize: "8px", fontFamily: "Arial", fontWeight: "bold" }}>Important Information</Text>
+              <View style={{
+                marginTop: "5px",
+
+
+              }}>
+                <Text style={{
+                  fontWeight: "bold",
+                  fontFamily: "Arial",
+                  fontSize: "7px"
+
+                }}> CONTINUOUS INSURANCE ENFORCEMENT and the MOTOR INSURANCE DATABASE </Text><Text style={{
+                  fontSize: "6px"
+                }} > Information relating to your policy will be added to the Motor Insurance Database ('MID') managed by the Motor
+                  Insurance Bureau ('MIB'). MID and the data stored on it may be used by certain statutory and/or authorised bodies including the Police, the DVLA, the DVLANI, the Insurance Fraud Bureau and other bodies
+                  permitted by law for purposes not limited to but including:
+                </Text>
+                <View style={{ margin: "10px 0px", display: "flex", gap: "3px" }}>
+                  <View style={{ display: "flex", flexDirection: "row", gap: "15px" }}><Text style={{ textAlign: "right", width: "30px" }} >I</Text><Text>Electronic Licensing</Text></View>
+                  <View style={{ display: "flex", flexDirection: "row", gap: "15px" }}><Text style={{ textAlign: "right", width: "30px" }} >II</Text><Text>Continuous Insurance Enforcement</Text></View>
+                  <View style={{ display: "flex", flexDirection: "row", gap: "15px" }}><Text style={{ textAlign: "right", width: "30px" }} >III</Text><Text>Law enforcement (prevention, detection, apprehension and or prosecution of offenders)</Text></View>
+                  <View style={{ display: "flex", flexDirection: "row", gap: "15px" }}><Text style={{ textAlign: "right", width: "30px" }} >IV</Text><Text>The provision of government services and or other services aimed at reducing the level and incidence of uninsured driving.</Text></View>
+
+
+                </View>
+                <Text style={{ lineHeight: "1.3px" }}>
+                  If you are involved in a road traffic accident (either in the UK, EEA or certain other territories), insurers and or the MIB may search the MID to
+                  obtain relevant information.
+                </Text>
+                <Text style={{ lineHeight: "1.3px" }}>
+                  Persons (including his or her appointed representatives) pursuing a claim in respect of a road traffic accident (including citizens of other countries)
+                  may also obtain information which is held on the MID. It is vital that the MID holds your correct registration number. If it is incorrectly shown on MID
+                  you are at risk of having your vehicle seized by the Police.
+
+                </Text>
               </View>
             </View>
           </View>
