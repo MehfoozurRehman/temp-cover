1baa730154362cc3a8adf00cb1ea6c7c3a4806bc
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-04 16:45:06 +0500

    Delete index.ts

--- src/assets/index.ts
+++
@@ -1,9 +0,0 @@
-import TtfArialbd from './arialbd.ttf'
-import PngFirstlogo from './firstlogo.png'
-import PngPdfsign from './pdfsign.png'
-
-export {
-  TtfArialbd,
-  PngFirstlogo,
-  PngPdfsign
-}
