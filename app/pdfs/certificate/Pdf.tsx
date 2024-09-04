b61fdc27977693d65da85240c9198d2b6b4eac4d
Asim Rasheed <asimrasheed442@gmail.com>
2024-09-04 11:09:21 +0500

    Update Pdf.tsx

--- app/pdfs/certificate/Pdf.tsx
+++ app/pdfs/certificate/Pdf.tsx
@@ -10,6 +10,9 @@ import {
 } from "@react-pdf/renderer";
 
 import React from "react";
+// import arialbd from "../../../public/arialbd.ttf";
+// import firstlogo from "../../assets/firstlogo.png";
+// import pdfsign from "../../assets/pdfsign.png";
 
 Font.register({
   family: "Arial",
@@ -58,7 +61,7 @@ export default function Pdf({ data }: { data: any }) {
               <Image style={{ width: "130px" }} src="/firstlogo.png" />
               <Text
                 style={{
-                  fontSize: "16px",
+                  fontSize: "14px",
                   fontFamily: "Arial",
                   fontWeight: "bold",
                 }}
@@ -107,7 +110,7 @@ export default function Pdf({ data }: { data: any }) {
             >
               <Text
                 style={{
-                  fontSize: "11px",
+                  fontSize: "9px",
                 }}
               >
                 Certificate Number:TCV-MOT-1739398
@@ -124,7 +127,7 @@ export default function Pdf({ data }: { data: any }) {
             >
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   fontFamily: "Arial",
                 }}
               >
@@ -133,7 +136,7 @@ export default function Pdf({ data }: { data: any }) {
 
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   paddingLeft: "10px",
                   fontFamily: "Arial",
                 }}
@@ -142,7 +145,7 @@ export default function Pdf({ data }: { data: any }) {
               </Text>
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   fontWeight: "medium",
                   paddingLeft: "5px",
                 }}
@@ -161,7 +164,7 @@ export default function Pdf({ data }: { data: any }) {
             >
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   fontFamily: "Arial",
                 }}
               >
@@ -170,7 +173,7 @@ export default function Pdf({ data }: { data: any }) {
 
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   paddingLeft: "10px",
                   fontFamily: "Arial",
                 }}
@@ -179,7 +182,7 @@ export default function Pdf({ data }: { data: any }) {
               </Text>
               <Text
                 style={{
-                  fontSize: "10px",
+                  fontSize: "9px",
                   fontWeight: "medium",
                   paddingLeft: "5px",
                 }}
@@ -206,7 +209,7 @@ export default function Pdf({ data }: { data: any }) {
               >
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     fontFamily: "Arial",
                   }}
                 >
@@ -215,7 +218,7 @@ export default function Pdf({ data }: { data: any }) {
 
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     paddingLeft: "10px",
                     fontFamily: "Arial",
                   }}
@@ -224,7 +227,7 @@ export default function Pdf({ data }: { data: any }) {
                 </Text>
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     fontWeight: "medium",
                     paddingLeft: "5px",
                   }}
@@ -243,7 +246,7 @@ export default function Pdf({ data }: { data: any }) {
               >
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     fontFamily: "Arial",
                   }}
                 >
@@ -251,7 +254,7 @@ export default function Pdf({ data }: { data: any }) {
                 </Text>
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     paddingLeft: "10px",
                     fontFamily: "Arial",
                   }}
@@ -260,7 +263,7 @@ export default function Pdf({ data }: { data: any }) {
                 </Text>
                 <Text
                   style={{
-                    fontSize: "10px",
+                    fontSize: "9px",
                     fontWeight: "medium",
                     paddingLeft: "5px",
                   }}
@@ -269,6 +272,187 @@ export default function Pdf({ data }: { data: any }) {
                 </Text>
               </View>
             </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingTop: "20px",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "9px",
+                  fontFamily: "Arial",
+                }}
+              >
+                (5)
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: "9px",
+                  paddingLeft: "10px",
+                  fontFamily: "Arial",
+                }}
+              >
+                Persons or Classes of Persons Entitled to Drive
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: "8px",
+                fontWeight: "medium",
+                paddingLeft: "20px",
+                paddingTop: "5px",
+              }}
+            >
+              (Provided that the person holds a licence to drive such a vehicle
+              and is not disqualified from holding or obtaining such a licence)
+            </Text>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingTop: "20px",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "9px",
+                  fontFamily: "Arial",
+                }}
+              >
+                (2)
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: "9px",
+                  fontWeight: "medium",
+                  paddingLeft: "10px",
+                }}
+              >
+                Mohammed Hasham Iqbal
+              </Text>
+            </View>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                flexDirection: "row",
+                paddingTop: "50px",
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: "9px",
+                  fontFamily: "Arial",
+                }}
+              >
+                (6)
+              </Text>
+
+              <Text
+                style={{
+                  fontSize: "9px",
+                  fontFamily: "Arial",
+                  paddingLeft: "10px",
+                }}
+              >
+                Limitations to use:
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: "9px",
+                paddingTop: "5px",
+                paddingLeft: "20px",
+              }}
+            >
+              Use for social, domestic and pleasure purposes and use in person
+              by the Policyholder in connection with their business or
+              profession EXCLUDING Commercial Travelling, hiring, carriage of
+              passengers or goods for hire and reward, any purpose in connection
+              with the Motor Trade, racing, pace-making, speed testing or
+              reliability testing or use on any race track, race circuit or toll
+              road without a speed limit including the Nurburgring
+            </Text>
+            <Text
+              style={{
+                fontSize: "9px",
+                paddingTop: "10px",
+                paddingLeft: "20px",
+                fontFamily: "Arial",
+              }}
+            >
+              This Certificate cannot be used as evidence of cover for the
+              purpose of recovering impounded vehicles.
+            </Text>
+            <Text
+              style={{
+                fontSize: "8px",
+                paddingTop: "10px",
+              }}
+            >
+              I hereby certify that the insurance to which this Certificate
+              relates satisfies the requirements of the related law applicable
+              in Great Britain, Northern Ireland, the Isle of Man, the Island of
+              Guernsey, the Island of Jersey and the Island of Alderney.
+            </Text>
+            <View
+              style={{
+                width: "100%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-between",
+                flexDirection: "row",
+                fontSize: "9px",
+                marginTop: "20px",
+                paddingLeft: "30px",
+                paddingRight: "30px",
+              }}
+            >
+              <View
+                style={{
+                  display: "flex",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <Text>
+                  Insured by First Underwriting Ltd for and {"\n"}on behalf of
+                  Accredited Insurance {"\n"} (Europe) Limited {"\n"}Authorised
+                  insurers
+                </Text>
+              </View>
+              <View>
+                <Image style={{ width: "90px" }} src={pdfsign} />
+              </View>
+              <View
+                style={{
+                  display: "flex",
+                  alignItems: "flex-start",
+                  justifyContent: "flex-start",
+                }}
+              >
+                <Text>
+                  Colin {"\n"}
+                  Johnson {"\n"}
+                  Director {"\n"}
+                  Accredited Insurance (Europe) Limited
+                </Text>
+              </View>
+            </View>
+          </View>
+        </Page>
+        <Page>
+          <View>
+            <Text>new</Text>
           </View>
         </Page>
       </Document>
