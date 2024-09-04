650933ef0c00ffa87bdba747e2be4528b53a3296
Asim Rasheed <asimrasheed442@gmail.com>
2024-09-04 11:09:42 +0500

    Create index.ts

---
+++ src/assets/index.ts
@@ -0,0 +1,9 @@
+import TtfArialbd from './arialbd.ttf'
+import PngFirstlogo from './firstlogo.png'
+import PngPdfsign from './pdfsign.png'
+
+export {
+  TtfArialbd,
+  PngFirstlogo,
+  PngPdfsign
+}
