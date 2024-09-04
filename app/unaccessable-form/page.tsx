f1fc6b0a14cff1469775f342859993228d2dfddd
Asim Rasheed <asimrasheed442@gmail.com>
2024-09-04 18:56:30 +0500

    Update page.tsx

--- app/unaccessable-form/page.tsx
+++ app/unaccessable-form/page.tsx
@@ -1,3 +1,48 @@
+import Image from "next/image";
+import secondLogo from "../../public/secondLogo.png";
+import "../style/form.scss";
 export default function Form() {
-  return <div>Form</div>;
+  return (
+    <>
+      <div className="from__main__container">
+        <form className="form__warper">
+          <div className="form__warper__logo">
+            <Image width={250} height={70} src={secondLogo} alt="logo" />
+          </div>
+          <div className="form__input__warper">
+            <div className="input__entry">
+              <div className="input__entry__label">Enter Number</div>
+              <input
+                className="input__entry__label__input"
+                type="text"
+                placeholder="Enter number"
+              />
+              <div className="input__entry__error">fill the input filed</div>
+            </div>
+            <div className="input__entry">
+              <div className="input__entry__label">Enter insured</div>
+              <input
+                className="input__entry__label__input"
+                type="text"
+                placeholder="Enter  insured"
+              />
+              <div className="input__entry__error">fill the input filed</div>
+            </div>
+          </div>
+          <div className="form__input__warper">
+            <div className="input__entry">
+              <div className="input__entry__label">Date Issued</div>
+              <input className="input__entry__label__input" type="date" />
+              <div className="input__entry__error">fill the input filed</div>
+            </div>
+            <div className="input__entry">
+              <div className="input__entry__label">Affective Date</div>
+              <input className="input__entry__label__input" type="date" />
+              <div className="input__entry__error">fill the input filed</div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
