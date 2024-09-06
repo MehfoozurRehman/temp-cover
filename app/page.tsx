d443c4efcf490d338a67ed75b1cb87648e85f2b2
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-06 14:49:22 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
 import PageUi from "./PageUi";
+import { notFound } from "next/navigation";
 
 export default function Public({ searchParams }: { searchParams: any }) {
   const { id } = searchParams;
+
+  if (!id) {
+    notFound();
+  }
 
   return <PageUi id={id} />;
 }
