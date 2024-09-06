eae7d726fb7ceb0ea886bda91ee72bf161cf7017
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-06 14:28:15 +0500

    Update page.tsx

--- app/pdfs/certificate/page.tsx
+++ app/pdfs/certificate/page.tsx
@@ -1,11 +1,37 @@
-import Pdf from "./Pdf";
+import dynamic from "next/dynamic";
+import prisma from "@/lib/prisma";
 
-export default function PDF({ searchParams }: { searchParams: any }) {
-  const { id } = searchParams;
-
-  return (
-    <div>
-      <Pdf data={{ id }} />
+const Pdf = dynamic(() => import("./Pdf"), {
+  ssr: false,
+  loading: () => (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      Loading...
     </div>
-  );
+  ),
+});
+
+export default async function PDF({ searchParams }: { searchParams: any }) {
+  try {
+    const { id } = searchParams;
+
+    const form = await prisma.form.findUnique({
+      where: { id: id },
+      select: { certificate: true },
+    });
+
+    if (!form) {
+      throw new Error("Certificate not found");
+    }
+
+    return <Pdf data={form.certificate} />;
+  } catch (error: any) {
+    return <div>{error.message}</div>;
+  }
 }
