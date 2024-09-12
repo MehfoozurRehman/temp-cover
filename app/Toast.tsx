da83c84df316bfb511c963a8e17c48f0af8895af
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-12 18:31:18 +0500

    Create Toast.tsx

---
+++ app/Toast.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import "react-toastify/dist/ReactToastify.css";
+
+import { ToastContainer } from "react-toastify";
+
+export default function Toast() {
+  return (
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={true}
+      stacked={true}
+    />
+  );
+}
