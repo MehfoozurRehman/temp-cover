359feb6df8ff589f257c89778cd7c2c7eecc0e14
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-12 19:08:25 +0500

    Update route.ts

--- app/api/email/route.ts
+++ app/api/email/route.ts
@@ -10,8 +10,10 @@ export async function POST(request: Request): Promise<NextResponse> {
   try {
     const { email, formId } = await request.json();
 
+    console.log(email, formId, "email, formId");
+
     if (!email || !formId) {
-      return NextResponse.json({ message: "Email and formId are required" });
+      return NextResponse.json({ error: "Email and formId are required" });
     }
 
     const formData = await prisma.form.findUnique({
@@ -31,13 +33,18 @@ export async function POST(request: Request): Promise<NextResponse> {
       },
     });
 
+    console.log(formData, "formData");
+
     if (!formData) {
       return NextResponse.json({ error: "Form not found" });
     }
 
+    const emailFrom = process.env.NEXT_PUBLIC_EMAIL as string | undefined;
+    const from = emailFrom ? emailFrom : "Acme <onboarding@resend.dev>";
+
     const res = await sendEmail({
-      from: "Acme <onboarding@resend.dev>",
-      to: ["your-email@example.com", email],
+      from,
+      to: emailFrom ? [email] : ["ravager009@gmail.com"],
       subject: "Your policy details from temp cover",
       react: Template({
         link: baseURl + `?id=${formId}`,
