b8350c26d1551fcca5c0b94e441d47c1918dc7c9
mehfoozurrehman <mehfoozijaz786@gmail.com>
2024-09-12 18:31:15 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./style/global.scss";
 
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import Script from "next/script";
+import Toast from "./Toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,12 +25,15 @@ export default function RootLayout({
           href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
           integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
         />
-        <script
+        <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js"
           integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        ></script>
+        />
       </head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Toast />
+        {children}
+      </body>
     </html>
   );
 }
