7f9a06da3baec276d1670079c552556fd19988d4
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-10-23 21:13:54 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Toast />
-        {/*{children}*/}
+        {children}
       </body>
     </html>
   );
